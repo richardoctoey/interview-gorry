b55519864bcf045832dc6608bf5a5102ce96d784
richard oey <richard.oey>
2021-12-08 00:05:53 +0700

    change json key

--- event/model.go
+++ event/model.go
@@ -3,15 +3,16 @@ package event
 import (
 	"errors"
 	"richardoctoey/interview-gorry/common"
+	"github.com/google/uuid"
 	"time"
 )
 
 type Event struct {
-	UUID string `gorm:"primaryKey,column:uuid" json:"uuid"`
+	UUID string `gorm:"primaryKey,column:uuid" json:"uuid,omitempty"`
 	Name string `gorm:"column:name" json:"name"`
-	Location string `gorm:"column:location" age:"location"`
-	StartTime time.Time `gorm:"column:start_time" age:"start_time"`
-	EndTime time.Time `gorm:"column:end_time" age:"end_time"`
+	Location string `gorm:"column:location" json:"location"`
+	StartTime time.Time `gorm:"column:start_time" json:"start_time"`
+	EndTime time.Time `gorm:"column:end_time" json:"end_time"`
 }
 
 func (u Event) TableName() string {
@@ -20,15 +21,16 @@ func (u Event) TableName() string {
 
 func (u *Event) Save() (error) {
 	if u.UUID == "" {
+		u.UUID = uuid.New().String()
 		return common.GetDb().Create(&u).Error
 	}
 
 	var count int64
-	common.GetDb().Model(&Event{}).Where("id = ?", u.UUID).Count(&count)
+	common.GetDb().Model(&Event{}).Where("uuid = ?", u.UUID).Count(&count)
 	if count != 1 {
 		return errors.New("Not exists")
 	}
-	return common.GetDb().Where("id = ?", u.UUID).Save(&u).Error
+	return common.GetDb().Where("uuid = ?", u.UUID).Save(&u).Error
 }
 
 func GetEvents() ([]Event, error) {
